c5f7c3119b8057abbafecd5a11e0ebbc631a8fdd
MP <MP@LINBLR3WX82Z2.internal.spherasolutions.com>
2020-12-02 16:50:52 +0530

    login test

---
+++ Test Suites/Login.ts
@@ -0,0 +1,47 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient>MP@sphera.com;</mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>b1ea3bf9-5b6f-479b-b901-269c1e1daa62</testSuiteGuid>
+   <testCaseLink>
+      <guid>ed43f565-f233-458c-b486-28a636701bcd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Sample test1</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>269ca295-293b-44ed-be0b-48ebb5afa7f3</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/data1</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>269ca295-293b-44ed-be0b-48ebb5afa7f3</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Username</value>
+         <variableId>2fcf2e31-3539-4df4-92df-5eabf2167a80</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>269ca295-293b-44ed-be0b-48ebb5afa7f3</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Password</value>
+         <variableId>025886c3-92ef-4d44-9971-b8f5a43247f1</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2d64383e-1ff4-46a0-b638-a03051a6b5e5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test sample 2</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
